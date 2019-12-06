8d9797bc3e56a7d869bb80894738035e44db3a2f
Erik Godding Boye <erik.boye@statnett.no>
2019-12-06 10:31:16 +0100

    Initial skeleton generated by Spring Initializr

---
+++ src/main/java/erikgb/fabric8/fmppr1763/FmpPr1763Application.java
@@ -0,0 +1,13 @@
+package erikgb.fabric8.fmppr1763;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmpPr1763Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmpPr1763Application.class, args);
+	}
+
+}
---
+++ src/test/java/erikgb/fabric8/fmppr1763/FmpPr1763ApplicationTests.java
@@ -0,0 +1,13 @@
+package erikgb.fabric8.fmppr1763;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmpPr1763ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
